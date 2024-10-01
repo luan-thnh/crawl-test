a4965d6026c133e52ac0f4ba2a512e029dc75143
luan-thnh <luanthanh0322@gmail.com>
2024-10-01 10:42:17 +0700

    feat: api-work

--- src/app/api/event/schedule/route.ts
+++ src/app/api/event/schedule/route.ts
@@ -1,9 +1,22 @@
 import dayjs from 'dayjs';
 import { NextResponse } from 'next/server';
+import chromium from '@sparticuz/chromium-min';
 import puppeteer from 'puppeteer-core';
 
+const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
+
 export const GET = async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    args: isLocal ? puppeteer.defaultArgs() : chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath:
+      process.env.CHROME_EXECUTABLE_PATH ||
+      (await chromium.executablePath(
+        `https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar`
+      )),
+    headless: chromium.headless,
+    ignoreDefaultArgs: ['--disable-extensions'],
+  });
   const page = await browser.newPage();
   await page.goto('https://bdc.consulting/calendar');
 
